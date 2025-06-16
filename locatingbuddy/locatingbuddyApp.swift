77319f951fe7027e733284b681450e5f043bc2c7
Toto <toto@laterravita.com>
2025-06-16 16:28:04 -0700

    Initial Commit

---
+++ locatingbuddy/locatingbuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  locatingbuddyApp.swift
+//  locatingbuddy
+//
+//  Created by Toto Poulos on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct locatingbuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ locatingbuddyTests/locatingbuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  locatingbuddyTests.swift
+//  locatingbuddyTests
+//
+//  Created by Toto Poulos on 6/16/25.
+//
+
+import Testing
+@testable import locatingbuddy
+
+struct locatingbuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
